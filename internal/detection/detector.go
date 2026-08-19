d1e1a8687d18fb1fd9c9312b526509a5ea9630f9
hami7-glitch <hamiraiesi007@gmail.com>
2026-08-20 02:37:05 +0330

    Update detector.go

--- internal/detection/detector.go
+++ internal/detection/detector.go
@@ -1,17 +1,47 @@
 package detection
 
-import "github.com/hami7-glitch/NetGhost/internal/model"
+import (
+	"fmt"
+	"time"
+
+	"github.com/hami7-glitch/NetGhost/internal/model"
+)
 
 type Alert struct {
 	Message string
 	Event   model.NetworkEvent
 }
 
-func Analyze(event model.NetworkEvent) *Alert {
-	if event.DestPort == 23 {
-		return &Alert{
-			Message: "Suspicious connection to Telnet port",
-			Event:   event,
+const (
+	portScanThreshold = 5
+	portScanWindow    = 10 * time.Second
+)
+
+func Analyze(events []model.NetworkEvent) *Alert {
+	if len(events) == 0 {
+		return nil
+	}
+
+	portsByIP := make(map[string]map[uint16]bool)
+
+	for _, event := range events {
+		if _, exists := portsByIP[event.SourceIP]; !exists {
+			portsByIP[event.SourceIP] = make(map[uint16]bool)
+		}
+
+		portsByIP[event.SourceIP][event.DestPort] = true
+	}
+
+	for sourceIP, ports := range portsByIP {
+		if len(ports) >= portScanThreshold {
+			return &Alert{
+				Message: fmt.Sprintf(
+					"Possible port scan detected from %s: %d different ports",
+					sourceIP,
+					len(ports),
+				),
+				Event: events[0],
+			}
 		}
 	}
 
