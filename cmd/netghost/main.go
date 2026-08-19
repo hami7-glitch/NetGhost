4e971f1ff1c4e7f24cc1f0908e3eb5cba8c05790
hami7-glitch <hamiraiesi007@gmail.com>
2026-08-20 02:10:14 +0330

    Update main.go

--- cmd/netghost/main.go
+++ cmd/netghost/main.go
@@ -1,8 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/hami7-glitch/NetGhost/internal/collector"
+)
 
 func main() {
 	fmt.Println("🛡️ NetGhost v0.1")
 	fmt.Println("Network monitoring system starting...")
+	fmt.Println()
+
+	event := collector.CollectSample()
+
+	fmt.Println("Network Event:")
+	fmt.Println("Source IP:", event.SourceIP)
+	fmt.Println("Destination IP:", event.DestIP)
+	fmt.Println("Destination Port:", event.DestPort)
+	fmt.Println("Protocol:", event.Protocol)
+	fmt.Println("Timestamp:", event.Timestamp)
 }
